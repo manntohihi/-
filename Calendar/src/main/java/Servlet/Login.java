db78198a2896c203e9e0059bff2c6f724c620a1c
Imamura4 <ovs246148@kcsf2014.onmicrosoft.com>
2026-01-29 11:15:55 +0900

    a

--- Calendar/src/main/java/Servlet/Login.java
+++ Calendar/src/main/java/Servlet/Login.java
@@ -18,20 +18,7 @@ import model.User;
  @WebServlet("/login")
 public class Login extends HttpServlet {
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
 
-        request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-
-          // ユーザーが入力した値を取得
-        int id = Integer.parseInt(request.getParameter("id"));
-        String password = request.getParameter("password");
-        String name = request.getParameter("name");
-        int icon = Integer.parseInt(request.getParameter("icon"));
-         //Userインスタンス作成
-        User user = new User(id, password,name, icon);
         
 
     }
